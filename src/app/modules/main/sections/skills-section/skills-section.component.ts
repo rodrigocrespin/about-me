c16052daa16c5ce60def487c5cd89f29c822bc6e
Rodrigo <rodcrespin@gmail.com>
2021-06-24 01:46:56 -0300

    skills images url fix (#5)

--- src/app/modules/main/sections/skills-section/skills-section.component.ts
+++ src/app/modules/main/sections/skills-section/skills-section.component.ts
@@ -14,13 +14,13 @@ interface SkillModel {
 })
 export class SkillsSectionComponent {
   skills: SkillModel[] = [
-    { description: '.NET 5', imgUrl: '/assets/img/dotnet.svg' },
-    { description: 'Angular', imgUrl: '/assets/img/angular.png' },
-    { description: 'Docker', imgUrl: '/assets/img/docker.png' },
-    { description: 'NgRx', imgUrl: '/assets/img/ngrx.svg' },
-    { description: 'RabbitMQ', imgUrl: '/assets/img/rabbitmq.svg', textSize: 'small' },
-    { description: 'MySQL', imgUrl: '/assets/img/mysql.png' },
-    { description: 'MongoDb', imgUrl: '/assets/img/mongo.png', textSize: 'small' },
-    { description: 'Git', imgUrl: '/assets/img/git.png' },
+    { description: '.NET 5', imgUrl: 'assets/img/dotnet.svg' },
+    { description: 'Angular', imgUrl: 'assets/img/angular.png' },
+    { description: 'Docker', imgUrl: 'assets/img/docker.png' },
+    { description: 'NgRx', imgUrl: 'assets/img/ngrx.svg' },
+    { description: 'RabbitMQ', imgUrl: 'assets/img/rabbitmq.svg', textSize: 'small' },
+    { description: 'MySQL', imgUrl: 'assets/img/mysql.png' },
+    { description: 'MongoDb', imgUrl: 'assets/img/mongo.png', textSize: 'small' },
+    { description: 'Git', imgUrl: 'assets/img/git.png' },
   ];
 }
